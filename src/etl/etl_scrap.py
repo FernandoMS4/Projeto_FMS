10a0e1cce0865b4e4566b0cf7c13feb42d59f8b3
FernandoMS4 <fernando381046@gmail.com>
2025-01-28 16:15:01 -0300

    update: adição da nova estrutura de páginas

--- src/etl/etl_scrap.py
+++ src/etl/etl_scrap.py
@@ -31,6 +31,7 @@ def format_scrapy_amazon():
     df.to_csv('data/dados_tratados.csv', header=True, index=False)
     return df
 
+
 def format_scrapy_mercado_livre():
     """
     Lê o arquivo coletado json e realiza o tratamento dos dados e padroniza de acordo com a tabela de produtos
@@ -41,28 +42,48 @@ def format_scrapy_mercado_livre():
         df = pd.read_json(
             'D:\PROJETO_WEBSCRAP_FMS\\archive\produtos_250127_184950.jsonl',
             dtype='str',
-            lines=True
-            )
+            lines=True,
+        )
     except KeyError as e:
         return print(f'Não foi possível ler o arquivo: {e}')
-    
+
     df = df.drop_duplicates()
 
     df['reviews'] = df['reviews'].astype('float')
 
-    df['reviews_qtd'] = df['reviews_qtd'].str.replace('(',' ').str.replace(')',' ').astype('int')
+    df['reviews_qtd'] = (
+        df['reviews_qtd']
+        .str.replace('(', ' ')
+        .str.replace(')', ' ')
+        .astype('int')
+    )
 
-    df['product_price_from'] = (df['product_price_from_fraction'].astype('str').str.replace('.','') + '.' + df['product_price_from_cents'].astype('str')).astype('float')
+    df['product_price_from'] = (
+        df['product_price_from_fraction'].astype('str').str.replace('.', '')
+        + '.'
+        + df['product_price_from_cents'].astype('str')
+    ).astype('float')
 
-    df['product_price_to'] = (df['product_price_to'].astype('str').str.replace('.','') + '.' + df['product_price_to_cents'].astype('str')).astype('float')
+    df['product_price_to'] = (
+        df['product_price_to'].astype('str').str.replace('.', '')
+        + '.'
+        + df['product_price_to_cents'].astype('str')
+    ).astype('float')
 
     df['marketplace'] = 'mercado livre'
 
-    df = df.drop(columns=['product_price_from_fraction','product_price_from_cents','product_price_to_cents'])
+    df = df.drop(
+        columns=[
+            'product_price_from_fraction',
+            'product_price_from_cents',
+            'product_price_to_cents',
+        ]
+    )
     return df
 
+
 if __name__ == '__main__':
-    #df = format_scrapy_amazon()
-    #df.to_csv('data/dados_tratados.csv', header=True, index=False)
+    # df = format_scrapy_amazon()
+    # df.to_csv('data/dados_tratados.csv', header=True, index=False)
     df = format_scrapy_mercado_livre()
     print(df)
